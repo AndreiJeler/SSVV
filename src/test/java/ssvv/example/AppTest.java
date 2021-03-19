47616a451222b0017121a191ae803cd4d46f188e
Andrei Jeler <42837811+SummerRolls99@users.noreply.github.com>
2021-03-19 20:00:12 +0200

    added the setup method

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -4,34 +4,61 @@ import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ssvv.example.domain.*;
 import ssvv.example.repository.*;
 import ssvv.example.service.*;
 import ssvv.example.validation.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private StudentXMLRepository studentRepo;
+    private NotaXMLRepository notaRepo;
+    private TemaXMLRepository temaRepo;
+
+    private Service service;
+
+    @Before
+    public void setup(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+
+        studentRepo = new StudentXMLRepository(studentValidator, "testing/studenti.xml");
+        notaRepo = new NotaXMLRepository(notaValidator, "testing/note.xml");
+        temaRepo = new TemaXMLRepository(temaValidator, "testing/teme.xml");
+        service = new Service(studentRepo,temaRepo,notaRepo);
+
+        try {
+            String defaultFileContent = new String(Files.readAllBytes(Paths.get("testing/empty-studenti.xml")), StandardCharsets.UTF_8);
+
+            PrintWriter printWriter = new PrintWriter("testing/studenti.xml");
+
+            printWriter.print(defaultFileContent);
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Test
     public void testAdd1() {
-        Validator<Student> studentValidator = new StudentValidator();
-        StudentRepository repository = new StudentRepository(studentValidator);
-        Student s = repository.save(new Student("1", "John", 934));
-        assertEquals("John", repository.findOne("1").getNume());
+        int r = service.saveStudent("1", "John", 934);
+        assertEquals("John", studentRepo.findOne("1").getNume());
+        assertEquals(1, r);
     }
 
     @Test
@@ -42,4 +69,6 @@ public class AppTest
         Student s = repository.save(new Student("", "John", 934));
         assertNull(s);
     }
+
+
 }
